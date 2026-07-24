ffab7bdf01fefaaaf59c0c6061274c31f45de36f
Kairav <kairavdutta@gmail.com>
2026-07-24 13:40:40 +0530

    [CLI] : added more command-recognition to cli

--- src/cli/cli_entry.zig
+++ src/cli/cli_entry.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-
+const memeql = std.mem.eql;
 // -------------- temp functions created until the actual ones are -------------------------
 
 fn cbuild(args: anytype) void {
@@ -29,14 +29,30 @@ pub fn handle_cli(init: std.process.Init.Minimal) !void {
         std.debug.print("Please enter the command as a command-line argument\n", .{});
         std.process.exit(1);
     }
-    if (std.mem.eql(u8, command.?, "build")) {
+    if (memeql(u8, command.?, "build")) {
         cbuild(args);
-    } else if (std.mem.eql(u8, command.?, "run")) {
+    } else if (memeql(u8, command.?, "run")) {
         run(args);
-    } else if (std.mem.eql(u8, command.?, "add")) {
+    } else if (memeql(u8, command.?, "add")) {
         add(args);
-    } else if (std.mem.eql(u8, command.?, "init")) {
+    } else if (memeql(u8, command.?, "init")) {
         c_init(args);
+    } else if (memeql(u8, command.?, "test")) {
+        // run test
+    } else if (memeql(u8, command.?, "help") or memeql(u8, command.?, "h")) {
+        // run help
+    } else if (memeql(u8, command.?, "clean")) {
+        // clean function called
+    } else if (memeql(u8, command.?, "explain")) {
+        // diagnostics run
+    } else if (memeql(u8, command.?, "graph")) {
+        // run graph
+    } else if (memeql(u8, command.?, "doctor")) {
+        // run doctor
+    } else if (memeql(u8, command.?, "fmt")) {
+        // run fmt
+    } else if (memeql(u8, command.?, "check")) {
+        // run check
     } else {
         std.debug.print("Command not found : {s}\n", .{command.?});
     }
