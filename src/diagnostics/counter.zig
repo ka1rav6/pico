9096e3ed126ee712002d4a3f6b1e7ded6b085da2
Kairav <kairavdutta@gmail.com>
2026-07-25 02:43:02 +0530

    [Other] : Formatted all the files using zig fmt

--- src/diagnostics/counter.zig
+++ src/diagnostics/counter.zig
@@ -6,7 +6,7 @@ const event_count = @typeInfo(Event).Enum.fields.len;
 pub const Counter = struct {
     counts: [event_count]u32,
     pub fn init() Counter {
-        return .{ .counts = [_]u32{0}**event_count };
+        return .{ .counts = [_]u32{0} * *event_count };
     }
     pub fn increment(self: *Counter, event: Event) void {
         self.counts[@intFromEnum(event)] += 1;
@@ -15,6 +15,6 @@ pub const Counter = struct {
         return self.counts[@intFromEnum(event)];
     }
     pub fn reset(self: *Counter) void {
-        self.counts = [_]u32{0}**event_count;
+        self.counts = [_]u32{0} * *event_count;
     }
 };
