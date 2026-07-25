4032f90f4ede3a7ca25bce1b45a1b5be0f8ecbf4
Kairav <kairavdutta@gmail.com>
2026-07-25 21:37:40 +0530

    [TOML] : created a proper lexer struct (header only)

--- src/utils/toml_parser/lexer.h
+++ src/utils/toml_parser/lexer.h
@@ -1,18 +1,27 @@
 #ifndef PICO_TOML_LEXER_H
 #define PICO_TOML_LEXER_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "token.h"
+#include <stdbool.h>
+#include <stddef.h>
 
-typedef struct Lexer {
-  char *text;
-  size_t curr;
+#define LEXER_MAX_TOKENS 4096
+
+typedef struct {
+  const char *src;
+  size_t src_len;
+  size_t pos;
+  size_t line;
+  size_t col;
+  Token tokens[LEXER_MAX_TOKENS];
+  size_t token_count;
+  bool has_error;
+  char error_msg[256];
+  size_t error_line;
+  size_t error_col;
 } Lexer;
 
+void lexer_init(Lexer *l, const char *src, size_t src_len);
+bool lexer_tokenize(Lexer *l);
 
-
-
-
-
-#endif // PICO_TOML_LEXER_H
+#endif /* PICO_TOML_LEXER_H */
