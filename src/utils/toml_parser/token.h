8cabcd49b429d25e31c63264d7e9927f9dfa8e95
Kairav <kairavdutta@gmail.com>
2026-07-25 21:36:28 +0530

    [TOML] : added rest of the tokentypes and created proper token

--- src/utils/toml_parser/token.h
+++ src/utils/toml_parser/token.h
@@ -4,23 +4,81 @@
 #include <stddef.h>
 
 typedef enum {
-  DECIMAL_LITERAL,
-  FLOAT_LITERAL,
-  STRING_LITERAL,
-  BOOL_LITERAL,
-  EQUAL_SIGN,
-  LSQUARE,
-  RSQUARE,
-  IDENT,
-  COMMA,
-  DOT,
-  EOF_TOKEN,
+  // Literals
+  TOK_INTEGER,
+  TOK_FLOAT,
+  TOK_STRING,
+  TOK_BOOL,
+  TOK_DATETIME,
+
+  // Identifiers & keys
+  TOK_IDENT,
+
+  // Delimiters
+  TOK_EQUAL,
+  TOK_DOT,
+  TOK_COMMA,
+  TOK_COLON,
+
+  // Brackets
+  TOK_LBRACKET,  // [
+  TOK_RBRACKET,  // ]
+  TOK_LLBRACKET, // [[
+  TOK_RRBRACKET, // ]
+  TOK_LBRACE,    // {
+  TOK_RBRACE,    // }
+
+  // Special
+  TOK_NEWLINE,
+  TOK_EOF,
 } TokenType;
+
+static const char *token_type_name(TokenType t) {
+  switch (t) {
+  case TOK_INTEGER:
+    return "INTEGER";
+  case TOK_FLOAT:
+    return "FLOAT";
+  case TOK_STRING:
+    return "STRING";
+  case TOK_BOOL:
+    return "BOOL";
+  case TOK_DATETIME:
+    return "DATETIME";
+  case TOK_IDENT:
+    return "IDENT";
+  case TOK_EQUAL:
+    return "EQUAL";
+  case TOK_DOT:
+    return "DOT";
+  case TOK_COMMA:
+    return "COMMA";
+  case TOK_COLON:
+    return "COLON";
+  case TOK_LBRACKET:
+    return "LBRACKET";
+  case TOK_RBRACKET:
+    return "RBRACKET";
+  case TOK_LLBRACKET:
+    return "LLBRACKET";
+  case TOK_RRBRACKET:
+    return "RRBRACKET";
+  case TOK_LBRACE:
+    return "LBRACE";
+  case TOK_RBRACE:
+    return "RBRACE";
+  case TOK_NEWLINE:
+    return "NEWLINE";
+  case TOK_EOF:
+    return "EOF";
+  }
+  return "UNKNOWN";
+}
 
 typedef struct Token {
   TokenType type;
-  const char *lexeme;
-  size_t lexeme_len;
+  const char *start;
+  size_t length;
   size_t line;
   size_t col;
 } Token;
